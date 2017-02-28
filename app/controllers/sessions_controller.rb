7447614c1b9e5e73647ad0cd574166f153e66466
Luke Philips and Tyler Ferrin <tyler@>
2017-02-28 12:26:58 -0800

    SUP WORLD

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+
+class SessionsController < ApplicationController
+  def create
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      flash[:success] = "Sup, #{@user.name}!"
+    rescue
+      flash[:warning] = "I am broken..."
+    end
+    redirect_to root_path
+  end
+
+  def auth_failure
+    redirect_to root_path
+  end
+
+  def destroy
+    if current_user
+      session.delete(:user_id)
+      flash[:success] = 'Kick rocks!'
+    end
+    redirect_to root_path
+  end
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,17 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    scope: 'public_profile', info_fields: 'id,name,link'
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"], :skip_jwt => true,
+    scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name:
+    'google'
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'],
+    scope: 'r_basicprofile',
+    fields: ['id', 'first-name', 'last-name', 'location', 'picture-url', 'public-profile-url']
+end
+
+OmniAuth.config.on_failure do |env|
+  error_type = env['omniauth.error.type'],
+  new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{error_type}"
+  [301, {'Location' => new_path, 'Content-Type' => 'text/html'}, []]
+end
